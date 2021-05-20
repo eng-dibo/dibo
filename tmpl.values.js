0ad7561001271e3e1474a0b5b1223075cb79d1c4
eng-dibo <sh.eldeeb.2010@gmail.com>
2021-05-20 21:38:34 +0000

    feat: add values and config for tmpl

---
+++ tmpl.values.js
@@ -0,0 +1,18 @@
+export default {
+  scope: "engineers",
+  repo: "https://github.com/eng-dibo/dibo",
+  npm: "https://www.npmjs.com/org/engineers",
+  email: "sh.eldeeb.2010+dev.github@gmail.com",
+  author: "Sherif Eldeeb",
+  license: "MIT",
+  funding: [
+    {
+      type: "paypal",
+      url: "https://paypal.me/group99001",
+    },
+    {
+      type: "patreon",
+      url: "https://www.patreon.com/GoogleDev",
+    },
+  ],
+};
