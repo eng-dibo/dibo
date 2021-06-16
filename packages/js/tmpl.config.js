a90c353c33170fa11162faeaa0c0c556decefe59
eng-dibo <sh.eldeeb.2010@gmail.com>
2021-06-16 22:01:33 +0000

    build(js): add keywords

--- packages/js/tmpl.config.js
+++ packages/js/tmpl.config.js
@@ -1,5 +1,6 @@
 // merge this values with tmpl.values.js in workspace's root and packages' root
 export default function (tmplPath, config) {
   config.description = "javascript utils";
+  config.keywords = ["javascript", "js"];
   return config;
 }
