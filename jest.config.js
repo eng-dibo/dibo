5d3dd887c3d66d815e7feda1d02f69bb91c8a88a
eng-dibo <sh.eldeeb.2010@gmail.com>
2021-06-29 09:46:18 +0000

    build: jest config
    - modify jest.config
    - remove jest.config.js from packages/*
    - use test:all for CI tests

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,15 @@
+// https://jestjs.io/docs/cli
+
 module.exports = {
+  // use ts-jest to compile test files written in typescript
+  // alternatively, run `tsc` to compile .ts files first.
   preset: "ts-jest",
   testEnvironment: "node",
+  // don't inject jest methods (test,describe,...) to the global scope
+  // you must import them from '@jest/globals
+  injectGlobals: false,
+  // run tests for only files that changed from the last commit
+  onlyChanged: true,
+  // output the coverage report (--coverage in cli)
+  collectCoverage: false,
 };
--- packages/javascript/jest.config.js
+++
@@ -1,5 +0,0 @@
-// todo: use jest.config.js in the workspace's root dir
-// or use tmpl to generate this file
-
-const config = require("../../jest.config");
-module.exports = config;
