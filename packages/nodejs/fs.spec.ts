2f13e77158d2caf9bd215467cac75574d8614a6f
eng-dibo <sh.eldeeb.2010@gmail.com>
2021-07-07 19:58:37 +0000

    feat(nodejs): fs the async version of fs-sync

---
+++ packages/nodejs/fs.spec.ts
@@ -0,0 +1,119 @@
+/**
+ * all test cases must be in the same order as ./fs-sync.spec.ts
+ * and includes exactly the same test cases.
+ */
+
+import { test, expect, describe, jest } from '@jest/globals';
+import { resolve } from './fs-sync';
+import {
+  mkdir,
+  write,
+  getSize,
+  isDir,
+  getModifiedTime,
+  move,
+  read,
+  remove,
+} from './fs';
+import { existsSync } from 'fs';
+import { objectType } from '@engineers/javascript/objects';
+
+let dir = resolve(__dirname, './test'),
+  file1 = `${dir}/file.txt`,
+  file2 = `${dir}/file2.txt`,
+  file3 = `${dir}/file3.txt`,
+  file4 = `${dir}/file4.txt`,
+  fileJson = `${dir}/file.json`,
+  fileJsonComments = `${dir}/file-comments.json`,
+  fileArray = `${dir}/file-array.json`;
+
+test('mkdir', () => {
+  expect(existsSync(dir)).toBeFalsy();
+  return mkdir(dir).then((value) => expect(existsSync(dir)).toBeTruthy());
+});
+
+test('write', () => {
+  expect(existsSync(file1)).toBeFalsy();
+  expect(existsSync(fileJson)).toBeFalsy();
+  expect(existsSync(fileArray)).toBeFalsy();
+  return Promise.all([
+    write(file1, 'this file is generated during testing'),
+    write(file2, 'this file is generated during testing'),
+    write(file4, 'this file is generated during testing'),
+    write(fileJson, { x: 1, y: 2 }),
+    write(fileArray, [1, 2, 3]),
+    write(
+      fileJsonComments,
+      `// this file is created to test reading .json files that contains comments
+       // to test stripComments()
+  
+    {
+      /* it should remove all comments */
+      /* even this 
+           multi-line comment 
+        */
+      // also this comment
+
+      "x": 1,
+      "hello": "ok"
+    }
+    `
+    ),
+  ]).then((value) => {
+    expect(existsSync(file1)).toBeTruthy();
+    expect(existsSync(fileJson)).toBeTruthy();
+    expect(existsSync(fileArray)).toBeTruthy();
+  });
+});
+
+test('getSize', () =>
+  Promise.all([getSize(file1), getSize(dir)]).then((value) =>
+    expect(value).toEqual([37, 4096])
+  ));
+
+test('isDir', () =>
+  Promise.all([isDir(file1), isDir(dir)]).then((value) =>
+    expect(value).toEqual([false, true])
+  ));
+
+test('getModifiedTime -> file', () =>
+  Promise.all([getModifiedTime(file1), getModifiedTime(dir)]).then((value) => {
+    expect(Math.floor(value[0])).toBeGreaterThanOrEqual(1624906832178);
+    expect(Math.floor(value[1])).toBeGreaterThanOrEqual(1624906832178);
+  }));
+
+test('move', () => {
+  expect(existsSync(file2)).toBeTruthy();
+  return move(file2, file3).then(() => {
+    expect(existsSync(file2)).toBeFalsy();
+    expect(existsSync(file3)).toBeTruthy();
+  });
+});
+
+test('read', () =>
+  Promise.all([
+    read(file1),
+    read(fileJson),
+    read(fileJsonComments),
+    read(fileArray),
+  ]).then((value) => {
+    let [txt, json, jsonWithComments, arr] = value;
+    expect(txt.length).toEqual(37);
+    expect(txt).toContain('this file is generated during testing');
+    expect(objectType(txt)).toEqual('string');
+    expect(objectType(json)).toEqual('object');
+    expect(objectType(jsonWithComments)).toEqual('object');
+    expect(objectType(arr)).toEqual('array');
+    expect(json).toEqual({ x: 1, y: 2 });
+    expect(jsonWithComments).toEqual({ x: 1, hello: 'ok' });
+    expect(arr).toEqual([1, 2, 3]);
+  }));
+
+test('remove dir', () => {
+  expect(existsSync(file1)).toBeTruthy();
+  expect(existsSync(dir)).toBeTruthy();
+  return remove([dir]).then(() => {
+    expect(existsSync(file1)).toBeFalsy();
+    expect(existsSync(dir)).toBeFalsy();
+  });
+});
---
+++ packages/nodejs/fs.ts
@@ -0,0 +1,177 @@
+/**
+ * the promise version of `fs-sync`
+ * refer to fs-sync for docs and functions descriptions
+ */
+
+import {
+  promises as fsp,
+  PathLike,
+  constants,
+  MakeDirectoryOptions,
+  WriteFileOptions,
+  Abortable,
+} from 'fs';
+import {
+  MoveOptions,
+  RemoveOptions,
+  resolve,
+  ReadOptions,
+  stripComments,
+} from './fs-sync';
+import { basename } from 'path';
+import { objectType, Obj } from '@engineers/javascript/objects';
+import stripJsonComments from 'strip-json-comments';
+// todo: import { lstat } from 'fs/promises';
+
+/**
+ * get file size asynchronously
+ */
+export function getSize(
+  path: PathLike,
+  unit: 'b' | 'kb' | 'mb' | 'gb' = 'b'
+): Promise<number> {
+  return fsp
+    .lstat(path)
+    .then((stats: any) => stats.size)
+    .then((size: any) => {
+      let units = {
+        b: 0,
+        kb: 1,
+        mb: 2,
+        gb: 3,
+      };
+
+      return size / 1024 ** units[unit];
+    });
+}
+
+export function isDir(path: PathLike): Promise<boolean> {
+  return fsp.lstat(path).then((stats: any) => stats.isDirectory());
+}
+
+export function getModifiedTime(file: PathLike): Promise<number> {
+  return fsp.lstat(file).then((stats: any) => stats.mtimeMs);
+}
+
+export function mkdir(
+  path: string | string[],
+  mode: number | string = 0o777
+): Promise<void> {
+  if (path instanceof Array) {
+    return Promise.all(
+      path.map((p) => ({ [p]: mkdir(p, mode) }))
+    ).then(() => {});
+  }
+
+  let options: MakeDirectoryOptions = { mode, recursive: true };
+
+  return fsp
+    .access(path, constants.R_OK)
+    .catch(() => fsp.mkdir(path as string, options as MakeDirectoryOptions))
+    .then(() => {});
+}
+
+export function move(
+  path: PathLike,
+  newPath: PathLike,
+  options?: MoveOptions
+): any {
+  return fsp.rename(path, newPath);
+}
+
+/* todo: return Promise<boolean>
+   todo: overlaps:
+     export function remove(path: PathLike, options?: RemoveOptions): Promise<boolean>;
+     export function remove(path: PathLike[], options?: RemoveOptions): Promise<{ [path: string]: any }>;
+*/
+export function remove(
+  path: PathLike | PathLike[],
+  options: RemoveOptions = {}
+): Promise<void> {
+  if (!path) {
+    return Promise.reject('path not provided');
+  }
+
+  if (path instanceof Array) {
+    return Promise.all(
+      // todo: path.map((p) => ({ [p]: remove(p as string, options) }))
+      path.map((p) => remove(p, options))
+    ).then((value) => {
+      /* a void return, to make it compatible with the return type */
+    });
+  }
+
+  // todo: using `path` causes an issue
+  // i.e: path = resolve(path);
+  // https://github.com/microsoft/TypeScript/issues/44921
+  // https://stackoverflow.com/questions/68283677/typescript-has-different-types-for-the-same-variable-in-the-same-scope
+  // https://stackoverflow.com/questions/68240094/variable-type-inside-then-chain
+
+  let _path = resolve(path);
+  let opts = Object.assign({}, { keepDir: false }, options);
+  isDir(_path).then(() => isDir(_path));
+
+  return fsp
+    .access(_path, constants.R_OK)
+    .then(() => isDir(_path))
+    .then((_isDir: boolean) => {
+      return !_isDir
+        ? fsp.unlink(_path)
+        : fsp
+            .readdir(_path)
+            .then((files: any[]) =>
+              Promise.all(
+                files.map((file) => {
+                  return remove(`${_path}/${file}`, opts);
+                })
+              )
+            )
+            .then(() => (!opts.keepDir ? fsp.rmdir(_path) : Promise.resolve()));
+    })
+
+    .then(() => {});
+  // todo: or {file: boolean}
+  // .then(() => true)
+  // .catch(() => false)
+}
+
+export function write(
+  path: PathLike,
+  data: any,
+  // options for fs.promises.writeFile() is same as fs.writeFileSync() + Abortable{signal:..}
+  options?: WriteFileOptions & Abortable
+): Promise<void> {
+  path = resolve(path);
+  return mkdir(basename(path))
+    .then(() =>
+      ['array', 'object'].includes(objectType(data))
+        ? JSON.stringify(data)
+        : data
+    )
+    .then((dataString) => fsp.writeFile(path, dataString, options));
+  // .then-> {file,data}
+}
+
+export function read(
+  path: PathLike,
+  options?: ReadOptions
+): string | Array<any> | Obj {
+  let defaultOptions: ReadOptions = {
+    mode: 'txt',
+    encoding: null,
+    flag: 'r',
+  };
+  let opts: ReadOptions = Object.assign({}, defaultOptions, options);
+
+  return fsp
+    .readFile(path, {
+      encoding: opts.encoding,
+      flag: opts.flag,
+    })
+    .then((data) => data.toString())
+    .then((data) =>
+      opts.mode === 'json' || path.toString().trim().slice(-5) === '.json'
+        ? JSON.parse(stripJsonComments(data))
+        : data
+    );
+}
