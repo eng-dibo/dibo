eb9672bc8cf33bbab0531a674fc23fa999840c56
eng-dibo <sh.eldeeb.2010@gmail.com>
2021-08-22 21:35:31 +0000

    build(jest): uss typescript in jest.config

--- jest.config.ts
+++ jest.config.ts
@@ -1,28 +1,29 @@
 // https://jestjs.io/docs/cli
+import type { Config } from '@jest/types';
 
 // run 'ngcc' https://thymikee.github.io/jest-preset-angular/docs/guides/angular-ivy/
-require("jest-preset-angular/ngcc-jest-processor");
+import 'jest-preset-angular/ngcc-jest-processor';
 
-let { pathsToModuleNameMapper } = require("ts-jest/utils");
-let stripJsonComments = require("strip-json-comments");
+import pathsToModuleNameMapper from 'ts-jest/utils';
+import stripJsonComments from 'strip-json-comments';
+import fs from 'fs';
 
-const fs = require("fs");
 // todo: use `import { read } from "@engineers/nodejs/fs-sync.read()"`
-function readJson(path) {
+function readJson(path: string): any {
   let content = fs.readFileSync(path).toString();
   // strip comments from json file
   return JSON.parse(stripJsonComments(content));
 }
 
-let tsConfig = readJson("./tsconfig.json");
+let tsConfig = readJson('./tsconfig.json');
 let { compilerOptions } = tsConfig;
 
-module.exports = {
+let config: Config.InitialOptions = {
   // use 'ts-jest' to enable type checking while testing
   // use 'jest-preset-angular' for angular projects (built in top of ts-jest)
   // 'jest-preset-angular' requires tsconfig.spec.json file
-  preset: "jest-preset-angular",
-  testEnvironment: "node",
+  preset: 'jest-preset-angular',
+  testEnvironment: 'node',
   // don't inject jest methods (test,describe,...) to the global scope
   // you must import them from '@jest/globals
   injectGlobals: false,
@@ -30,24 +31,24 @@ module.exports = {
   onlyChanged: true,
   // output the coverage report (--coverage in cli)
   collectCoverage: false,
-  moduleDirectories: ["node_modules", "types"],
+  moduleDirectories: ['node_modules', 'types'],
   moduleFileExtensions: [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node",
-    "html",
-    "scss",
-    "css",
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'node',
+    'html',
+    'scss',
+    'css',
   ],
   transform: {
     // transform non-js files with 'jest-preset-angular' to let jest understand their syntax
     // so it can compile angular component's template and style files
     // https://thymikee.github.io/jest-preset-angular/docs/getting-started/options/#exposed-configuration
     // https://github.com/thymikee/jest-preset-angular/issues/992?notification_referrer_id=MDE4Ok5vdGlmaWNhdGlvblRocmVhZDIzMTMyODI4NTE6NTczMDg1MzE%3D#issuecomment-902427868
-    "^.+\\.(ts|js|html)$": "jest-preset-angular",
+    '^.+\\.(ts|js|html)$': 'jest-preset-angular',
   },
   /*
    pathsToModuleNameMapper generates moduleNameMapper from tsconfig.compilerOptions.paths
@@ -61,8 +62,8 @@ module.exports = {
   }),*/
 
   moduleNameMapper: {
-    "~~(.*)": "<rootDir>/$1",
-    "@engineers/(.*)": "<rootDir>/packages/$1",
+    '~~(.*)': '<rootDir>/$1',
+    '@engineers/(.*)': '<rootDir>/packages/$1',
     // this causes error: Could not locate module ./lib/source-map-generator
     // https://github.com/kulshekhar/ts-jest/issues/2718
     // "(.*)": "<rootDir>/node_modules/$1",
@@ -70,11 +71,12 @@ module.exports = {
 
   // jest setups for each testing file,
   // for example: preparing the testing environment
-  setupFilesAfterEnv: ["<rootDir>/jest-setup.ts"],
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
 
   // todo: causes error: AggregatedResult must be present after test run is complete
   // watch: true,
 };
+export default config;
 
 /*
  // to change 'ts-jest' options
