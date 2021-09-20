5c0de931aa643963d3fd1754cb702aefd0f78d86
eng-dibo <sh.eldeeb.2010@gmail.com>
2021-09-20 18:48:11 +0000

    init(express-redirect-middleware): redirect req
    - reqirect the request to https and www.

---
+++ packages/express-redirect-middleware/index.ts
@@ -0,0 +1,47 @@
+import { parseDomain, ParseResultListed } from 'parse-domain';
+
+export interface Options {
+  // force a specified protocol
+  protocol?: 'http' | 'https';
+  // add a subdomain if the domain doesn't have one
+  subdomain?: string;
+}
+
+/**
+ * redirects the request to the https version,
+ * and adds www. to the naked domains
+ * @param options
+ * @returns
+ */
+export default function (options?: Options): any {
+  return (req: any, res: any, next: any) => {
+    let parts: ParseResultListed = parseDomain(
+      req.hostname
+    ) as ParseResultListed;
+    // ex: www.example.com.eg ->{subDomains:[www], domain:google, topLevelDomains:[com]};
+    // old(v2.3.4):{domain:example, subdomain:www, tld:.com.eg}
+    // if the url cannot parsed (ex: localhost), parts= null, so we just skip to the next() middliware
+
+    if (parts && !['localhost', '127.0.0.1'].includes(parts.hostname)) {
+      let defaultOptions = {
+        protocol: req.secure ? 'https' : 'http',
+        subdomain: 'www',
+      };
+
+      let opts = Object.assign(defaultOptions, options || {});
+
+      // add a subdomain
+      if (!parts.subDomains || parts.subDomains === []) {
+        parts.subDomains = [opts.subdomain];
+      }
+
+      let url = `${opts.protocol}://${parts.subDomains.join('.')}.${
+        parts.domain
+      }.${(parts.topLevelDomains || []).join('.')}${req.url}`;
+
+      // todo: if(url != originalUrl)redirect
+      return res.redirect(301, url);
+    }
+    next();
+  };
+}
