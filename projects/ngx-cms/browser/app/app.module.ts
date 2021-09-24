e453b0fa7ea1be96565802d6bddb3a4faae71fa8
Dibo <sh.eldeeb.2010@gmail.com>
2021-09-24 22:03:33 +0000

    feat(ngx-cms): app routes

--- projects/ngx-cms/browser/app/app.module.ts
+++ projects/ngx-cms/browser/app/app.module.ts
@@ -5,14 +5,40 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ContentModule } from './content/content.module';
 import { ErrorComponent } from './error/error.component';
+import { Routes, RouterModule, InitialNavigation } from '@angular/router';
+import env from '~config/env';
+
+/*
+routes are loaded in the following order:
+ 1- appRoutes: all routes in the project, except endRoutes, contentRoutes (dynamic routes)
+ 2- contentRoutes comes after appRoutes because it contains dynamic paths ex: /:type
+ 3- endRoutes: contains the routes that must be loaded after all other routs, 
+    such as "**" (i.e: error component)
+ -> Modules are proceeded befor RouterModule.forRoot() and RouterModule.forChild()
+    we need to load AppRutingModule first then routes defineded in ContentModule (contains RouterModule.forChild())
+    then endRoutes in the last (because it contains '**')
+ -> @NgModule processes before RouterModule.forRoot()
+ https://blogs.msmvps.com/deborahk/angular-route-ordering/
+ */
+const appRoutes: Routes = [
+  //  { path: "", component: AppComponent, pathMatch: "full" }
+];
+const endRoutes: Routes = [{ path: '**', component: ErrorComponent }];
+const enableTracing = env.mode === 'development';
 
 @NgModule({
   declarations: [AppComponent, ErrorComponent],
   imports: [
+    RouterModule.forRoot(appRoutes, {
+      initialNavigation: <InitialNavigation>'enabled',
+      enableTracing,
+    }),
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
-    BrowserAnimationsModule,
     ContentModule,
+    RouterModule.forRoot(endRoutes, { enableTracing }),
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
