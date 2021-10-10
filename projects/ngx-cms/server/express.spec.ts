3f4a5014e4f113508a55dff93155c33b6b3d40c5
eng-dibo <sh.eldeeb.2010@gmail.com>
2021-10-10 19:28:11 +0000

    test(gx-cms): test express requests

--- projects/ngx-cms/server/express.spec.ts
+++ projects/ngx-cms/server/express.spec.ts
@@ -1,7 +1,8 @@
-import { test, expect, beforeAll } from '@jest/globals';
+import { test, expect, beforeAll, describe } from '@jest/globals';
 import { server } from './express';
 import { Express } from 'express';
 import EventEmitter from 'events';
+import supertest from 'supertest';
 
 let app: Express;
 beforeAll(() => {
@@ -10,6 +11,23 @@ beforeAll(() => {
 test('server', () => {
   expect(app).toBeTruthy();
   expect(app.constructor).toEqual(EventEmitter);
+});
+
+// testing routes via SuperTest
+// https://dev.to/lukekyl/testing-your-express-js-backend-server-3ae6
+// todo: fix: jest transpiles this file in place, so __dirname will give a different value
+// than its value in the transpiled file via tsc or webpack because te output path is different
+describe('routes', () => {
+  test('/collections', () => {
+    return supertest(app)
+      .get('/collections')
+      .then((response) => {
+        console.log({ response });
+        expect(response.status).toEqual(200);
+        expect(response.type).toEqual(expect.stringContaining('json'));
+        expect(response.body.message).toEqual('pass!');
+      });
+  });
 });
 
 // todo: test app.use(*)
