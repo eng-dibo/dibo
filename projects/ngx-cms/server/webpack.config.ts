219d76e31192d5ad67a99beba2716a3b24ff7a3e
eng-dibo <sh.eldeeb.2010@gmail.com>
2021-10-17 14:04:15 +0000

    fix(ngx-cms): webpack fix Unexpected token
    fix error: Unexpected token 'export'

--- projects/ngx-cms/server/webpack.config.ts
+++ projects/ngx-cms/server/webpack.config.ts
@@ -26,4 +26,13 @@ let config: Configuration = deepMerge([
   ) as { [key: string]: any }
 ).options!.configFile = resolve(__dirname, './tsconfig.json');
 
-export default config;
+// todo: a temporary workaround to fix: `Unexpected token 'export'` caused by `_asyncToGenerator(fn)`
+// this error occurs when express.ts or any of its dependencies has an async function definition
+// and configs merged by `@angular-builders/custom-webpack:server`
+// exporting a function prevents custom-webpack from merging configs using asyncToGenerator from babel
+function configFn(originalConfig: any): any {
+  return deepMerge([originalConfig, config]);
+}
+
+// export default config;
+export default configFn;
