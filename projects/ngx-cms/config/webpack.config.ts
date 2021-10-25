aed95cd7edf23ee9fde53ea073dff6c9ba634475
eng-dibo <sh.eldeeb.2010@gmail.com>
2021-10-25 11:13:11 +0000

    build(ngx-cms): webpack config
    - add config files to externals

--- projects/ngx-cms/config/webpack.config.ts
+++ projects/ngx-cms/config/webpack.config.ts
@@ -20,6 +20,25 @@ let config: Configuration = deepMerge([
     output: {
       path: resolve(__dirname, '../../../dist/ngx-cms/config'),
     },
+    // add config files to externals to prevent bundling them
+    // i.e: server should require('./database') instead of including its code while compiling
+    // each entry point must find its dependencies in the dist folder,
+    // i.e: when compiling ./server.ts, dist/database.js must be exists
+    // externals: Object.values(entries),
+    externals: [
+      (params: any, cb: any) => {
+        if (
+          // only config files
+          params.context === resolve(__dirname) &&
+          params.request.startsWith('./')
+        ) {
+          // console.log({ params });
+          cb(null, `commonjs2 ${params.request}`);
+        } else {
+          cb();
+        }
+      },
+    ],
   },
 ]);
 
