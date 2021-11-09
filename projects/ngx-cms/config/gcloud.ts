ff75ae1f11cc7586201d86fbd4f13dc0b446a624
Dibo <sh.eldeeb.2010@gmail.com>
2021-11-09 23:19:55 +0000

    feat(ngx-cms): deploy
    - add config/gcloud
    - fix deploy task

--- projects/ngx-cms/config/gcloud.ts
+++ projects/ngx-cms/config/gcloud.ts
@@ -1,1 +1,23 @@
-export const projectId = process.env.gcloud_projectId || 'dibo-cloud';
+export interface GCloudConfig {
+  projectId: string;
+  cloudRun: CloudRun;
+}
+
+export interface CloudRun {
+  name: string;
+  platform: string;
+  region: string;
+  allowUnauthenticated: boolean;
+}
+
+const config: GCloudConfig = {
+  projectId: process.env.gcloud_projectId || 'dibo-cloud',
+  cloudRun: {
+    name: 'ngx-cms',
+    platform: 'managed',
+    region: 'europe-west1',
+    allowUnauthenticated: true,
+  },
+};
+
+export default config;
--- projects/ngx-cms/tasks/deploy.ts
+++ projects/ngx-cms/tasks/deploy.ts
@@ -1,19 +1,33 @@
 import { basename, resolve } from 'path';
 import { ncp } from 'ncp';
-import { projectId } from '~config/gcloud';
+import gcloudConfig, { GCloudConfig } from '~config/gcloud';
 import { execSync } from '@engineers/nodejs/process';
-let image = `gcr.io/${projectId}/ngx-cms`;
-
-let rootPath = resolve(__dirname, '../../..'),
-  projectPath = resolve(__dirname, '..');
+import { remove } from '@engineers/nodejs/fs';
 
 /**
  * build a docker image and deploy it to gcloud run
  * gcloud must be installed, run task: setup
+ *
+ * @param options overrides gcloudConfig
  */
 // todo: detect if gcloud not installed, run task: setup
-export default function (): Promise<any> {
-  let destination = `${rootPath}/dist/ngx-cms`;
+export default function (options?: GCloudConfig): Promise<any> {
+  let opts: GCloudConfig = Object.assign(
+    {
+      runInstance: {
+        name: 'cms-run',
+        platform: 'managed',
+        region: 'europe-west1',
+        allowUnauthenticated: true,
+      },
+    },
+    gcloudConfig || {},
+    options || {}
+  );
+  let image = `gcr.io/${gcloudConfig.projectId}/ngx-cms`,
+    rootPath = resolve(__dirname, '../../..'),
+    projectPath = resolve(__dirname, '..'),
+    destination = `${rootPath}/dist/ngx-cms`;
 
   // copy the required files to build the container image
   return Promise.all(
@@ -21,12 +35,17 @@ export default function (): Promise<any> {
       `${projectPath}/Dockerfile`,
       `${projectPath}/package.json`,
       `${rootPath}/package-lock.json`,
-    ].map((file) => copy(file, `${destination}/${basename(file)}`))
+    ].map((file) =>
+      remove(`${destination}/${basename(file)}`).then(() =>
+        copy(file, `${destination}/${basename(file)}`)
+      )
+    )
   )
     .then(() => {
       console.log('login to gcloud');
       // todo: only if not signed
       // use `gcloud auth list --filter=status:ACTIVE --format="value(account)"`
+      // todo: auto auth in ci (i.e: github actions)
       execSync('gcloud auth login');
 
       console.log(`> building the image ${image} ...`);
@@ -37,7 +56,13 @@ export default function (): Promise<any> {
 
       console.log('> deploying ...');
       execSync(
-        `gcloud run deploy cms-run --image=${image} --platform=managed --port=4200 --region=europe-west1 --allow-unauthenticated`
+        `gcloud run deploy ${
+          opts.cloudRun.name
+        } --image=${image} --port=4200 --project=${opts.projectId} --platform=${
+          opts.cloudRun.platform
+        } --region=${opts.cloudRun.region} ${
+          opts.cloudRun.allowUnauthenticated ? '--allow-unauthenticated' : ''
+        }`
       );
 
       console.log('Done');
@@ -45,10 +70,16 @@ export default function (): Promise<any> {
     .catch((err) => console.log({ err }));
 }
 
+/**
+ * converts `ncp` into a promise
+ * @param source
+ * @param destination
+ * @returns
+ */
 function copy(source: string, destination: string): Promise<void> {
-  // console.log({ source, destination });
   return new Promise((resolve, reject) => {
     ncp(source, destination, (error) => {
+      console.log(`${source} copied to ${destination}`);
       error ? reject(error) : resolve();
     });
   });
