092633ba6a7ee4ee6979349c3f03671ac81b25ff
Dibo <sh.eldeeb.2010@gmail.com>
2021-11-09 20:42:17 +0000

    feat(express-redirect-middleware): options
    - add options.cb
    - fix wrong newUrl
    - support utf encoded domains, using formUrl()
    - remove defaultOptions to keep the original values
    - fix package.json

--- packages/express-redirect-middleware/index.ts
+++ packages/express-redirect-middleware/index.ts
@@ -1,10 +1,18 @@
-import { parseDomain, ParseResultListed } from 'parse-domain';
+import { fromUrl, parseDomain, ParseResultListed } from 'parse-domain';
+import url from 'url';
 
 export interface Options {
-  // force a specified protocol
+  // force a specified protocol, if !protocol: keep the original protocol
   protocol?: 'http' | 'https';
   // add a subdomain if the domain doesn't have one
   subdomain?: string;
+  // a function that called if the url changed and being redirected
+  cb?: (
+    oldUrl: string,
+    newUrl: string,
+    parts: ParseResultListed,
+    req: any
+  ) => void;
 }
 
 /**
@@ -13,36 +21,45 @@ export interface Options {
  * @param options
  * @returns
  */
+// todo: testing (use supertest)
 export default function (options?: Options): any {
   return (req: any, res: any, next: any) => {
     let parts: ParseResultListed = parseDomain(
-      // or req.get('host')
-      req.hostname
+      // fromUrl() gets the punycode of the domain,
+      // use url.domainToUnicode() to convert it back to unicode
+      // https://www.npmjs.com/package/parse-domain
+      // https://nodejs.org/api/url.html#urldomaintounicodedomain
+      // https://www.npmjs.com/package/punycode (deprecated)
+      fromUrl(req.hostname)
     ) as ParseResultListed;
     // ex: www.example.com.eg ->{subDomains:[www], domain:google, topLevelDomains:[com]};
-    // old(v2.3.4):{domain:example, subdomain:www, tld:.com.eg}
-    // if the url cannot parsed (ex: localhost), parts= null, so we just skip to the next() middliware
+    // if the url cannot parsed (ex: localhost), parts= null, so we just skip to the next() middleware
+    // @ts-ignore
+    parts.domain = url.domainToUnicode(parts.domain);
 
     if (parts && !['localhost', '127.0.0.1'].includes(parts.hostname)) {
-      let defaultOptions = {
-        protocol: 'https',
-        subdomain: 'www',
-      };
+      let opts = options || {};
 
-      let opts = Object.assign(defaultOptions, options || {});
-
-      // add a subdomain
-      if (!parts.subDomains || parts.subDomains === []) {
+      // if no subdomain, add opts.subdomain
+      if (opts.subdomain && (!parts.subDomains || parts.subDomains === [])) {
         parts.subDomains = [opts.subdomain];
       }
 
-      let url = `${opts.protocol}://${parts.subDomains.join('.')}.${
+      let subdomain =
+        parts.subDomains.length > 0 ? parts.subDomains.join('.') + '.' : '';
+
+      let newUrl = `${opts.protocol || req.protocol}://${subdomain}${
         parts.domain
       }.${(parts.topLevelDomains || []).join('.')}${req.originalUrl}`;
 
+      let oldUrl = `${req.protocol}://${req.hostname}${req.originalUrl}`;
+
       // redirect only if the url has been changed to prevent circular redirects
-      if (url !== `${req.protocol}://${req.hostname}${req.originalUrl}`) {
-        return res.redirect(301, url);
+      if (newUrl !== oldUrl) {
+        if (opts.cb && typeof opts.cb === 'function') {
+          opts.cb(oldUrl, newUrl, parts, req);
+        }
+        return res.redirect(301, newUrl);
       }
     }
     next();
