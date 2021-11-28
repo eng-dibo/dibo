69744830f94b0c23c08a034f8bd0359cd6f4236a
eng-dibo <sh.eldeeb.2010@gmail.com>
2021-11-28 19:43:04 +0000

    build(ngx-cms): tasks/config
    - choose wheter to bumdle config with webpack
     or run with ts-node
     - control the build process via env.BUILD_* variables

--- projects/ngx-cms/tasks/build.ts
+++ projects/ngx-cms/tasks/build.ts
@@ -16,13 +16,20 @@ export type Mode = 'production' | 'development' | 'test';
 export interface BuildOptions {
   targets?: string | Array<string>;
   mode?: Mode;
+  // for buildConfig
+  // if `js`, use webpack to generate js bundles
+  // otherwise, copy files from source as .ts, use `node-ts`to run the server
+  // webpack produces a non-human readable output, as an alternative use ts-node to run from .ts files directly
+  // example: > `cross-env BUILD_OUTPUT=js npm run task -- build --targets=config`
+  output?: 'ts' | 'js';
 }
 
 export default function (options?: BuildOptions): void {
   let opts = Object.assign(
     {
-      targets: 'browser,server,config,package',
+      targets: process.env.BUILD_TARGETS || 'browser,server,config,package',
       mode: process.env.NODE_ENV || 'production',
+      output: process.env.BUILD_OUTPUT || 'ts',
     },
     options || {}
   );
@@ -38,7 +45,7 @@ export default function (options?: BuildOptions): void {
     buildServer(opts.mode);
   }
   if (targets.includes('config')) {
-    buildConfig();
+    buildConfig(opts.output);
   }
   if (targets.includes('package')) {
     buildPackage();
@@ -65,22 +72,22 @@ export function buildServer(mode: Mode = 'production'): void {
   write(`${destination}/core/server/info.json`, { mode, time });
 }
 
-export function buildConfig(): void {
-  // now the source code(.ts) cold used in config using `ts-node` instead of `webpack`
-  // no need to compile it
-  // i.e read config/*.ts using ts-node for more readability
-  // webpack produces non-human readable output
-  // execSync(`webpack -c config/webpack.config.ts`);
-  // todo: if error: Promise.reject()
-
+export function buildConfig(output?: string): void {
   console.log(`> build config`);
 
   ['browser', 'server'].forEach((target) => {
     mkdir([`${destination}/config/${target}`]);
 
-    let files = readdirSync(`${projectPath}/config/${target}`);
-    // user-specific files (i.e file!!.ext, file!!) overrides project files (i.e file.ext)
-    let userFiles = files.filter((el) => /!!(\..+)?$/.test(el));
+    let files = readdirSync(`${projectPath}/config/${target}`),
+      // user-specific files (i.e file!!.ext, file!!) overrides project files (i.e file.ext)
+      userFiles = files.filter((el) => /!!(\..+)?$/.test(el));
+
+    if (output === 'js') {
+      execSync(`webpack -c config/webpack.config.ts`);
+
+      // copy non-ts files only
+      files = files.filter((el) => !el.endsWith('.ts'));
+    }
 
     files
       .filter(
@@ -88,8 +95,9 @@ export function buildConfig(): void {
           // get the corresponding project files to any existing user-specific files
           !userFiles.map((el) => el.replace('!!', '')).includes(el) &&
           lstatSync(`${projectPath}/config/${target}/${el}`).isFile()
-        // exclude files used for building
+        // exclude files used for building (i.e config files)
       )
+      .concat(userFiles)
       .forEach((el) =>
         copyFileSync(
           `${projectPath}/config/${target}/${el}`,
