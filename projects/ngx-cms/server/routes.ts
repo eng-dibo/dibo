89c60e5b530ebcb09c3894419d88bf3556e08361
eng-dibo <sh.eldeeb.2010@gmail.com>
2021-12-31 13:57:36 +0000

    feat(ngx-cms): server: restore
    - support multible hosts
    - support host pattern

--- projects/ngx-cms/server/routes.ts
+++ projects/ngx-cms/server/routes.ts
@@ -16,6 +16,7 @@ import { resolve, extname } from 'path';
 import { parse } from '@engineers/databases/operations';
 import { read, write } from './storage';
 import { existsSync, readdirSync, unlinkSync } from 'fs';
+import { toRegExp } from '@engineers/javascript/regex';
 
 let app = Router();
 const TEMP = resolve(__dirname, '../temp');
@@ -185,40 +186,68 @@ app.get('/backup', (req: any, res: any) => {
 });
 
 // before running this route: make any transformation (add/filter data) and drop any db if needed
-app.get('/restore', (req: any, res: any) => {
-  let dir = /*req.params.dir ||*/ req.query.dir;
-  if (!dir) {
-    return res.json({ error: 'provide ?dir' });
+app.get('/restore/:hosts?', (req: any, res: any) => {
+  let hosts = req.params.hosts;
+  let existingHosts = readdirSync(resolve(__dirname, `../temp/db-backup`));
+
+  if (!hosts) {
+    return res.json({
+      error: `
+      provide a host name or pattern (or multiple hosts), 
+      example: /restore/myhost.gbdqa.gcp.mongodb.net,myho.+  
+
+      available hosts:
+      ${existingHosts.map(
+        (el) => el.replace(resolve(__dirname, '../temp/db-backup'), '') + '\r\n'
+      )}
+      `,
+    });
   }
-  let dirPath = resolve(__dirname, `../temp/db-backup/${dir}`);
-  if (!existsSync) {
-    return res.json({ error: `the dir ${dirPath} not existing` });
-  }
-  return connect()
-    .then(() =>
-      Promise.all(
-        readdirSync(dirPath).map((file) => {
-          let filePath = resolve(`${dirPath}/${file}`);
-          if (extname(filePath) !== '.json') {
-            return;
+
+  connect().then(() =>
+    Promise.all(
+      existingHosts
+        .filter((host) => toRegExp(hosts.split(',')).test(host))
+        .map((host) => {
+          console.log(`> restoring from ${host}`);
+
+          let hostPath = resolve(__dirname, `../temp/db-backup/${host}`);
+          if (!existsSync) {
+            throw `the host ${host} not existing`;
           }
 
-          return (
-            readFS(filePath)
-              .then((content: any) => {
-                restore({ [file.replace('.json', '')]: content });
-              })
-              // todo: move .catch() to the top-level of Promise chain
-              .catch((error) => {
-                console.log(`[restore] error in restore()`, { error });
-                throw error;
-              })
+          // todo: sort by name
+          let backupPath = resolve(hostPath, readdirSync(hostPath)[0]);
+          if (backupPath.length === 0) {
+            console.warn(`no backup files for the host ${host}`);
+            Promise.resolve();
+          }
+
+          return Promise.all(
+            readdirSync(backupPath).map((file: string) => {
+              let filePath = resolve(`${backupPath}/${file}`);
+              if (extname(filePath) !== '.json') {
+                return;
+              }
+              console.log({ backupPath });
+              return (
+                readFS(filePath)
+                  .then((content: any) => {
+                    restore({ [file.replace('.json', '')]: content });
+                  })
+                  // todo: move .catch() to the top-level of Promise chain
+                  .catch((error) => {
+                    console.log(`[restore] error in restore()`, { error });
+                    throw error;
+                  })
+              );
+            })
           );
         })
-      )
     )
-    .then(() => res.json({ done: true }))
-    .catch((error) => res.json({ error }));
+      .then(() => res.json({ done: true }))
+      .catch((error) => res.json({ error }))
+  );
 });
 
 /*
