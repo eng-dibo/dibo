9e1a0cd1714b7b3827107a4f4d01d070b984066b
dibo <sh.eldeeb.2010@gmail.com>
2022-03-29 19:54:39 +0000

    feat(ngx-cms): purge cloudflare cache
    - purge cloudflare cache after deploying

--- projects/ngx-cms/tasks/deploy.ts
+++ projects/ngx-cms/tasks/deploy.ts
@@ -2,6 +2,7 @@ import gcloudConfig, { GCloudConfig } from '~config/server/gcloud';
 import { execSync } from '@engineers/nodejs/process';
 import { execSync as _execSync } from 'child_process';
 import setup from './setup';
+import https from 'node:https';
 
 /**
  * build a docker image and deploy it to gcloud run
@@ -69,6 +70,41 @@ export default function (options: GCloudConfig = {}): void {
       opts.cloudRun.allowUnauthenticated ? '--allow-unauthenticated' : ''
     }`
   );
+
+  console.log(`> purging cloudflare cache`);
+  // @ts-ignore
+  if (
+    cloudFlareConfig.purge &&
+    cloudFlareConfig.token &&
+    cloudFlareConfig.zone
+  ) {
+    let options = {
+      // https://stackoverflow.com/a/70377372/12577650
+      protocol: 'https:',
+      host: 'api.cloudflare.com',
+      path: `/client/v4/zones/${cloudFlareConfig.zone}/purge_cache`,
+      port: 443,
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${cloudFlareConfig.token}`,
+        'Content-Type': 'application/json',
+      },
+    };
+
+    var req = https.request(options, function (res) {
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+        console.log(chunk);
+      });
+    });
+
+    req.on('error', function (error) {
+      console.error(`> [cloudflare] error:`, error);
+    });
+
+    req.write(JSON.stringify(cloudFlareConfig.purge));
+    req.end();
+  }
 
   console.log('Done');
 }
