90fee81bf73cb63052216607c8236a74307664b7
dibo <sh.eldeeb.2010@gmail.com>
2022-03-30 19:14:55 +0000

    fix(ngx-content-view-mat): copy()
    - intro is the part before the first header or content's inner text
    - remove links from intro
    - headersText displayed as a list

--- packages/ngx-content-view-mat/article.ts
+++ packages/ngx-content-view-mat/article.ts
@@ -87,8 +87,10 @@ export class NgxContentArticleComponent {
     this.opts = Object.assign({}, this.options || {}, { quickActions });
   }
 
-  copy(card?: any) {
-    card = card || this.el.nativeElement;
+  copy(card?: HTMLElement) {
+    // clone the node element, so any modification (such as removing links)
+    // doesn't affect the original element
+    card = (card || this.el.nativeElement).cloneNode(true) as HTMLElement;
     let data: string | undefined;
 
     if (this.options && this.options!.copyAction) {
@@ -102,28 +104,51 @@ export class NgxContentArticleComponent {
 
     if (!data) {
       // todo: use referrer2
-      let title = card.getElementsByTagName('mat-card-title')[0],
-        titleText = title.textContent,
-        // todo: shorten link -> /$type/~$id
-        link = title.getElementsByTagName('a')[0].href;
+      let title = card.querySelector('mat-card-title'),
+        titleText = title?.textContent,
+        link = title?.querySelector('a')?.href;
 
       // shorten url
-      let url = new URL(link);
-      url.pathname = url.pathname.replace(
-        /([^\/]+)\/(?:[^\/]+)\/.+~([^\/?]+)/,
-        '$1/~$2'
-      );
-      link = url.href;
+      if (link) {
+        let url = new URL(link);
+        url.pathname = url.pathname.replace(
+          /([^\/]+)\/(?:[^\/]+)\/.+~([^\/?]+)/,
+          '$1/~$2'
+        );
+        link = url.href;
+      }
 
-      // todo: intro is the text before the first <h2> element
-      let content = card.getElementsByTagName('mat-card-content')[0],
-        intro = content.textContent.substr(0, 500),
-        headers = [...content.querySelectorAll('h2')]
-          .map((el) => el.textContent)
-          .join('\r\r');
+      let content = card.querySelector('.ql-editor') as HTMLElement;
+      if (content) {
+        let headers = [...(content?.querySelectorAll('h2') || [])],
+          headersText = headers
+            ? headers.map((el: HTMLElement) => `- ${el.textContent}`).join('\n')
+            : '',
+          intro;
 
-      // todo: use template, example: options.template='{{content.title}}\n{{content.link}}'
-      data = `${titleText}\n\n${intro}\n${headers} 👇👇\n${link}`;
+        // remove links from intro (or content)
+        content.querySelectorAll('a').forEach((el: HTMLElement) => el.remove());
+
+        if (headers.length > 0) {
+          // intro is all elements before the first header
+          let els = [];
+          let el = headers[0].previousElementSibling;
+          while (el) {
+            els.unshift(el as HTMLElement);
+            el = el.previousElementSibling;
+          }
+          intro = els.map((el) => el.innerText.trim()).join('\n');
+        } else {
+          intro = content?.innerText?.trim();
+        }
+
+        intro = intro.substr(0, 500).replace('\n\n', '\n');
+
+        // todo: use template, example: options.template='{{titleText}}\n{{link}}'
+        data = `${titleText}\n\n${intro}\n\n${headersText}\n\n👇👇\n${link}`;
+      } else {
+        data = `${titleText}\n${link}`;
+      }
     }
 
     if (navigator.clipboard) {
