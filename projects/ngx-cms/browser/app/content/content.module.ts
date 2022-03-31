e72b32832ba684013b74a72cfa1fcd300c65e8c4
dibo <sh.eldeeb.2010@gmail.com>
2022-03-31 16:46:41 +0000

    fix(ngx-cms): content.module routes

--- projects/ngx-cms/browser/app/content/content.module.ts
+++ projects/ngx-cms/browser/app/content/content.module.ts
@@ -13,7 +13,7 @@ import { UniversalInterceptor } from '@engineers/ngx-universal-express/universal
 const routes: Routes = [
   {
     matcher: (segments: any, group: any, route: any) =>
-      segments.length > 0 && segments[1].path === 'editor'
+      segments.length > 1 && segments[1].path === 'editor'
         ? // consume the first 2 segments only (i.e articles/editor), and keep the third segment (i.e /item)
           { consumed: segments.slice(0, 2) }
         : null,
@@ -24,7 +24,7 @@ const routes: Routes = [
   },
   {
     matcher: (segments: any, group: any, route: any) =>
-      segments.length > 0 && segments[1].path === 'manage'
+      segments.length > 1 && segments[1].path === 'manage'
         ? { consumed: segments.slice(0, 2) }
         : null,
     loadChildren: () =>
