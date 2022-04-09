b76eb0bb8fef30d79a628fea6e8f9fd77d488e64
dibo <sh.eldeeb.2010@gmail.com>
2022-04-09 15:43:44 +0000

    fix(social-control): toolbar

--- projects/social-control/config/browser/toolbar.js
+++ projects/social-control/config/browser/toolbar.js
@@ -1,7 +1,5 @@
 // todo: img.logo
 module.exports = [
-  { content: "jobs", link: "/jobs" },
-  { content: "articles", link: "/" },
   { content: "follow us", link: "/social" },
   { class: "spacer" },
   { content: "login", id: "member" },
