741beedb3ada552bf3b4c8ee05359762b76e81d8
dibo <sh.eldeeb.2010@gmail.com>
2022-04-10 09:26:43 +0000

    remove(ngx-cms): messenger platform
    - messenger platform featres moved to it's own project

--- projects/ngx-cms/config/server/models.js
+++ projects/ngx-cms/config/server/models.js
@@ -207,31 +207,3 @@ module.exports.push_notifications = {
   expirationTime: String,
   keys: { p256dh: String, auth: String },
 };
-
-// todo: implement login system
-module.exports.messenger_persons = {};
-
-module.exports.messenger_pages = {
-  // page id
-  _id: String,
-  // the access_token for this page
-  access_token: String,
-  user: { type: String, ref: "messenger_persons" },
-  // the greeting message that displays before the conversation starts
-  // https://developers.facebook.com/docs/messenger-platform/discovery/welcome-screen/
-  greeting: [{ locale: String, text: String }],
-  // the first message or block of messages that be sent to the user
-  // when he clicks the get started button
-  // could be any message template
-  welcome: mixed,
-  // the persistent menu
-  // https://developers.facebook.com/docs/messenger-platform/send-messages/persistent-menu/
-  menu: [mixed],
-};
-
-module.exports.messenger_blocks = {
-  // keep _id short to be used as postback for buttons
-  _id: { type: String, default: shortId.generate },
-  items: [{ service: { type: String, default: "message" }, payload: [mixed] }],
-  user: { type: String, ref: "messenger_persons" },
-};
