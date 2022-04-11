cf3c8e392b8c15ffe099d3e3c3584d78fe76e476
Dibo <sh.eldeeb.2010@gmail.com>
2022-04-11 22:50:02 +0000

    feat(social-control): config/gcloud: cloud run
    - cloud run name = container image name by default

--- projects/social-control/config/server/gcloud.js
+++ projects/social-control/config/server/gcloud.js
@@ -2,7 +2,10 @@ module.exports = {
   // todo: use dist/config/server/.env for deploying the production-ready project
   projectId: process.env.gcloud_projectId || "social-control",
   cloudRun: {
-    name: process.env.gcloud_cloudRun_name || "social-control",
+    name:
+      process.env.gcloud_cloudRun_name || process.env.gcloud_cloudRun_image
+        ? process.env.gcloud_cloudRun_image.replace(/\./, "-")
+        : "social-control",
     platform: process.env.gcloud_cloudRun_platform || "managed",
     region: process.env.gcloud_cloudRun_region || "europe-west1",
     allowUnauthenticated: true,
