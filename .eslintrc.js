d1f64630b6bf3f4a33f7cdb89f920b7f31f1f6a9
eng-dibo <sh.eldeeb.2010@gmail.com>
2022-05-16 01:32:17 +0200

    build: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,90 @@
+// todo: create a task to install missing eslint plugins and parsers (from top scope and overrides), run as postinstall
+// `npm i -D eslint-plugin-$plugin-name
+// plugins.map(el=>!el.startsWith('@')?el.startsWith('eslint-plugin')?el:'eslint-plugin-'+el: ..)
+
 module.exports = {
+  // lint all file types, add plugins or parsers for unsupported types
+  // todo: is this replaces the cli option `--ext` that is set to '.js' only by default?
+  files: ["**/*.*"],
   env: {
     browser: true,
     es6: true,
+    jest: true,
   },
+
+  // to search eslint plugins: https://www.npmjs.com/search?q=keywords%3Aeslint-plugin
+  // to learn more about a plugin https://www.npmjs.com/package/eslint-plugin-$pluginName
+  // you may apply recommended rules of each plugin
+  // by adding $plugin/recommended to extends[]
+  // as of projects and packages requirements, add more plugins
+  plugins: [
+    "@typescript-eslint/eslint-plugin",
+    // linting ES6 import/export syntax
+    // for example: Ensure imports point to a file/module that can be resolved
+    // todo: import VS require-path-exists
+    // https://github.com/import-js/eslint-plugin-import/issues/2452
+    "import",
+    "jsdoc",
+    // extends the basic eslint rules
+    // https://github.com/typescript-eslint/typescript-eslint/tree/main/packages/eslint-plugin/docs/rules
+    "@angular-eslint/eslint-plugin",
+    // linting Angular templates
+    "@angular-eslint/eslint-plugin-template",
+    // prefer arrow function
+    "prefer-arrow",
+    // best practices for regexp rules and avoid wrong regexp definitions
+    "regexp",
+    // removes the unused imports
+    "unused-imports",
+    "unicorn",
+    // supports require(), import and webpack aliases
+    "require-path-exists",
+    "json",
+    // linting package.json
+    "json-files",
+    // linting rules fore nodejs, forked from  eslint-plugin-node
+    "n",
+    // finds common security issues
+    "@microsoft/eslint-plugin-sdl",
+    // use 'const' only at the top-level of a module's scope, and 'let' anywhere else
+    // set the rule `prefer-const: off`
+    "prefer-let",
+    // searches for secrets
+    "no-secrets",
+    "security-node",
+    "yaml",
+    "anti-trojan-source",
+    // sort export statements
+    "sort-export-all",
+    // identify patterns that will interfere with the tree-shaking algorithm of their module bundler (i.e. rollup or webpack)
+    "tree-shaking",
+    // Detects when a module has been imported and not listed as a dependency in package.json.
+    "implicit-dependencies",
+    "@html-eslint",
+  ],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:jsdoc/recommended",
+    "plugin:regexp/recommended",
+    "plugin:unicorn/recommended",
+    "plugin:require-path-exists/recommended",
+    "plugin:json/recommended",
+    "plugin:n/recommended",
+    "plugin:@microsoft/sdl/recommended",
+    // disallows angular-bypass-sanitizer
+    // "plugin:@microsoft/sdl/angular",
+    "plugin:security-node/recommended",
+    "plugin:yaml/recommended",
+    "plugin:anti-trojan-source/recommended",
+    "plugin:sort-export-all/recommended",
+    // Google JavaScript style guide
+    // https://google.github.io/styleguide/jsguide.html
+    // plugin: https://npmjs.com/package/eslint-config-google
+    "google",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
@@ -22,219 +100,71 @@ module.exports = {
        https://stackoverflow.com/a/61959593/12577650
    */
 
-  // ignoring any non-standard file extensions
-  // including files without extensions such as `Dockerfile`
-  // to solve: he extension for the file () is non-standard
-  ignorePatterns: ["**/*.*", "**/*", "package-lock.json"],
-  // ignorePath: ".gitignore",
-  plugins: [
-    "eslint-plugin-import",
-    "eslint-plugin-jsdoc",
-    "@angular-eslint/eslint-plugin",
-    "@angular-eslint/eslint-plugin-template",
-    "eslint-plugin-prefer-arrow",
-    "@typescript-eslint",
-    "@typescript-eslint/tslint",
+  // ignoring any non-standard file extensions to solve `the extension for the file () is non-standard`
+  // including files without extensions such as `Dockerfile` "**/*",
+  ignorePatterns: ["**/*", "package-lock.json"],
+  ignorePath: ".gitignore",
+  // override configs for some files
+  overrides: [
+    {
+      // linting angular test files to follow best practices for testing
+
+      files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+      extends: [
+        "plugin:jest/recommended",
+        // todo: enable only for angular projects and packages (starts with ngx-*)
+        "plugin:testing-library/angular",
+      ],
+      plugins: ["jest", "testing-library"],
+    },
+    {
+      files: ["**/*.graphql"],
+      parser: "@graphql-eslint/eslint-plugin",
+      plugins: ["@graphql-eslint"],
+      rules: {
+        "@graphql-eslint/known-type-names": "error",
+      },
+    },
+    {
+      files: ["*.json", "*.json5", "*.jsonc"],
+      parser: "jsonc-eslint-parser",
+    },
+    {
+      files: ["*.ejs.*"],
+      // parse files as ejs code instead of normal js
+      // ejs files contains invalid js tokens
+      // example: `class <%= className %>{}`
+      plugins: ["ejs"],
+    },
+    {
+      files: ["*.html", "*.htm"],
+      parser: "@html-eslint/parser",
+      extends: ["plugin:@html-eslint/recommended"],
+    },
   ],
+
+  // https://eslint.org/docs/rules/
+  // also see docs for each plugin
+  // use https://www.npmjs.com/package/eslint-rule-docs to find docs for a rule
   rules: {
-    "@angular-eslint/component-class-suffix": "error",
-    "@angular-eslint/contextual-lifecycle": "error",
-    "@angular-eslint/directive-class-suffix": "error",
-    "@angular-eslint/no-conflicting-lifecycle": "error",
-    "@angular-eslint/no-host-metadata-property": "error",
-    "@angular-eslint/no-input-rename": "error",
-    "@angular-eslint/no-inputs-metadata-property": "error",
-    "@angular-eslint/no-output-native": "error",
-    "@angular-eslint/no-output-on-prefix": "error",
-    "@angular-eslint/no-output-rename": "error",
-    "@angular-eslint/no-outputs-metadata-property": "error",
-    "@angular-eslint/template/banana-in-box": "error",
-    "@angular-eslint/template/no-negated-async": "error",
-    "@angular-eslint/use-lifecycle-interface": "error",
-    "@angular-eslint/use-pipe-transform-interface": "error",
-    "@typescript-eslint/adjacent-overload-signatures": "error",
-    "@typescript-eslint/array-type": "off",
-    "@typescript-eslint/ban-types": [
-      "error",
-      {
-        types: {
-          Object: {
-            message: "Avoid using the `Object` type. Did you mean `object`?",
-          },
-          Function: {
-            message:
-              "Avoid using the `Function` type. Prefer a specific function type, like `() => void`.",
-          },
-          Boolean: {
-            message: "Avoid using the `Boolean` type. Did you mean `boolean`?",
-          },
-          Number: {
-            message: "Avoid using the `Number` type. Did you mean `number`?",
-          },
-          String: {
-            message: "Avoid using the `String` type. Did you mean `string`?",
-          },
-          Symbol: {
-            message: "Avoid using the `Symbol` type. Did you mean `symbol`?",
-          },
-        },
-      },
-    ],
-    "@typescript-eslint/consistent-type-assertions": "error",
-    "@typescript-eslint/dot-notation": "error",
-    "@typescript-eslint/indent": [
-      "error",
-      2,
-      {
-        CallExpression: {
-          arguments: "first",
-        },
-        ArrayExpression: "first",
-        ObjectExpression: "first",
-        FunctionDeclaration: {
-          parameters: "first",
-        },
-        FunctionExpression: {
-          parameters: "first",
-        },
-      },
-    ],
-    "@typescript-eslint/member-delimiter-style": [
-      "error",
-      {
-        multiline: {
-          delimiter: "semi",
-          requireLast: true,
-        },
-        singleline: {
-          delimiter: "semi",
-          requireLast: false,
-        },
-      },
-    ],
-    "@typescript-eslint/member-ordering": "error",
-    "@typescript-eslint/naming-convention": "error",
-    "@typescript-eslint/no-empty-function": "off",
-    "@typescript-eslint/no-empty-interface": "error",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-inferrable-types": [
-      "error",
-      {
-        ignoreParameters: true,
-      },
-    ],
-    "@typescript-eslint/no-misused-new": "error",
-    "@typescript-eslint/no-namespace": "error",
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/no-parameter-properties": "off",
-    "@typescript-eslint/no-shadow": [
-      "error",
-      {
-        hoist: "all",
-      },
-    ],
-    "@typescript-eslint/no-unused-expressions": "error",
-    "@typescript-eslint/no-use-before-define": "off",
-    "@typescript-eslint/no-var-requires": "off",
-    "@typescript-eslint/prefer-for-of": "error",
-    "@typescript-eslint/prefer-function-type": "error",
-    "@typescript-eslint/prefer-namespace-keyword": "error",
-    "@typescript-eslint/quotes": ["error", "single"],
-    "@typescript-eslint/semi": ["error", "always"],
-    "@typescript-eslint/triple-slash-reference": [
-      "error",
-      {
-        path: "always",
-        types: "prefer-import",
-        lib: "always",
-      },
-    ],
-    "@typescript-eslint/type-annotation-spacing": "error",
-    "@typescript-eslint/unified-signatures": "error",
-    "arrow-body-style": "error",
-    complexity: "off",
-    "constructor-super": "error",
-    curly: "error",
-    "eol-last": "error",
-    eqeqeq: ["error", "smart"],
-    "guard-for-in": "error",
-    "id-blacklist": [
-      "error",
-      "any",
-      "Number",
-      "number",
-      "String",
-      "string",
-      "Boolean",
-      "boolean",
-      "Undefined",
-      "undefined",
-    ],
-    "id-match": "error",
-    "import/no-deprecated": "warn",
-    "jsdoc/check-alignment": "error",
-    "jsdoc/check-indentation": "error",
-    "jsdoc/newline-after-description": "error",
-    "jsdoc/no-types": "off",
-    "max-classes-per-file": "off",
-    "max-len": [
-      "error",
-      {
-        code: 140,
-      },
-    ],
-    "new-parens": "error",
-    "no-bitwise": "error",
-    "no-caller": "error",
-    "no-cond-assign": "error",
-    "no-console": "off",
-    "no-debugger": "error",
-    "no-empty": "off",
-    "no-eval": "error",
-    "no-fallthrough": "error",
-    "no-invalid-this": "off",
-    "no-new-wrappers": "error",
-    "no-restricted-imports": ["error", "rxjs/Rx"],
-    "no-throw-literal": "error",
-    "no-trailing-spaces": "error",
-    "no-undef-init": "error",
-    "no-underscore-dangle": "error",
-    "no-unsafe-finally": "error",
-    "no-unused-labels": "error",
-    "no-var": "error",
-    "object-shorthand": "error",
-    "one-var": ["off", "never"],
-    "prefer-arrow/prefer-arrow-functions": "error",
+    // sort object keys alphabetically
+    // use eslint-plugin-sort-keys-fix to enable auto fixing
+    "sort-keys": ["warn"],
+    "prefer-let/prefer-let": 2,
     "prefer-const": "off",
-    "quote-props": ["error", "as-needed"],
-    radix: "error",
-    "space-before-function-paren": "off",
-    "spaced-comment": [
+    "no-secrets/no-secrets": "error",
+    "sort-imports": "warn",
+    "json-files/sort-package-json": "warn",
+    "json-files/ensure-repository-directory": "warn",
+    "json-files/require-engines": "warn",
+    // prevent duplicate packages in dependencies and devDependencies
+    "json-files/require-unique-dependency-names": "error",
+    // handle promises correctly with `await` or `.then()` and `.catch()`
+    "@typescript-eslint/no-floating-promise": "error",
+    "tree-shaking/no-side-effects-in-initialization": "error",
+    "implicit-dependencies/no-implicit": [
       "error",
-      "always",
-      {
-        markers: ["/"],
-      },
-    ],
-    "use-isnan": "error",
-    "valid-typeof": "off",
-    "@typescript-eslint/tslint/config": [
-      "error",
-      {
-        rules: {
-          "import-spacing": true,
-          typedef: [true, "call-signature"],
-          whitespace: [
-            true,
-            "check-branch",
-            "check-decl",
-            "check-operator",
-            "check-separator",
-            "check-type",
-            "check-typecast",
-          ],
-        },
-      },
+      { dev: true, peer: true, optional: true },
     ],
   },
 };
