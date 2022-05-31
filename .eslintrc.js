f358c5a307ccc45575308bf45630a3f4eca901a2
eng-dibo <sh.eldeeb.2010@gmail.com>
2022-05-31 17:51:26 +0200

    build: eslint
    - fix ignorePatterns
    - move parserOptions to overrides

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,9 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-  ],
+
   parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "tsconfig.json",
-    sourceType: "module",
-  },
+
   /* to solve the error: The file does not match your project config:
      this issue occurs when formatting a file not included in parserOptions.project (i.e: tsconfig.json)
      
@@ -25,7 +18,7 @@ module.exports = {
   // ignoring any non-standard file extensions
   // including files without extensions such as `Dockerfile`
   // to solve: he extension for the file () is non-standard
-  ignorePatterns: ["**/*.*", "**/*", "package-lock.json"],
+  ignorePatterns: ["package-lock.json"],
   // ignorePath: ".gitignore",
   plugins: [
     "eslint-plugin-import",
@@ -36,205 +29,18 @@ module.exports = {
     "@typescript-eslint",
     "@typescript-eslint/tslint",
   ],
-  rules: {
-    "@angular-eslint/component-class-suffix": "error",
-    "@angular-eslint/contextual-lifecycle": "error",
-    "@angular-eslint/directive-class-suffix": "error",
-    "@angular-eslint/no-conflicting-lifecycle": "error",
-    "@angular-eslint/no-host-metadata-property": "error",
-    "@angular-eslint/no-input-rename": "error",
-    "@angular-eslint/no-inputs-metadata-property": "error",
-    "@angular-eslint/no-output-native": "error",
-    "@angular-eslint/no-output-on-prefix": "error",
-    "@angular-eslint/no-output-rename": "error",
-    "@angular-eslint/no-outputs-metadata-property": "error",
-    "@angular-eslint/template/banana-in-box": "error",
-    "@angular-eslint/template/no-negated-async": "error",
-    "@angular-eslint/use-lifecycle-interface": "error",
-    "@angular-eslint/use-pipe-transform-interface": "error",
-    "@typescript-eslint/adjacent-overload-signatures": "error",
-    "@typescript-eslint/array-type": "off",
-    "@typescript-eslint/ban-types": [
-      "error",
-      {
-        types: {
-          Object: {
-            message: "Avoid using the `Object` type. Did you mean `object`?",
-          },
-          Function: {
-            message:
-              "Avoid using the `Function` type. Prefer a specific function type, like `() => void`.",
-          },
-          Boolean: {
-            message: "Avoid using the `Boolean` type. Did you mean `boolean`?",
-          },
-          Number: {
-            message: "Avoid using the `Number` type. Did you mean `number`?",
-          },
-          String: {
-            message: "Avoid using the `String` type. Did you mean `string`?",
-          },
-          Symbol: {
-            message: "Avoid using the `Symbol` type. Did you mean `symbol`?",
-          },
-        },
+  rules: {},
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      parserOptions: {
+        project: ["./tsconfig.json"],
       },
-    ],
-    "@typescript-eslint/consistent-type-assertions": "error",
-    "@typescript-eslint/dot-notation": "error",
-    "@typescript-eslint/indent": [
-      "error",
-      2,
-      {
-        CallExpression: {
-          arguments: "first",
-        },
-        ArrayExpression: "first",
-        ObjectExpression: "first",
-        FunctionDeclaration: {
-          parameters: "first",
-        },
-        FunctionExpression: {
-          parameters: "first",
-        },
-      },
-    ],
-    "@typescript-eslint/member-delimiter-style": [
-      "error",
-      {
-        multiline: {
-          delimiter: "semi",
-          requireLast: true,
-        },
-        singleline: {
-          delimiter: "semi",
-          requireLast: false,
-        },
-      },
-    ],
-    "@typescript-eslint/member-ordering": "error",
-    "@typescript-eslint/naming-convention": "error",
-    "@typescript-eslint/no-empty-function": "off",
-    "@typescript-eslint/no-empty-interface": "error",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-inferrable-types": [
-      "error",
-      {
-        ignoreParameters: true,
-      },
-    ],
-    "@typescript-eslint/no-misused-new": "error",
-    "@typescript-eslint/no-namespace": "error",
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/no-parameter-properties": "off",
-    "@typescript-eslint/no-shadow": [
-      "error",
-      {
-        hoist: "all",
-      },
-    ],
-    "@typescript-eslint/no-unused-expressions": "error",
-    "@typescript-eslint/no-use-before-define": "off",
-    "@typescript-eslint/no-var-requires": "off",
-    "@typescript-eslint/prefer-for-of": "error",
-    "@typescript-eslint/prefer-function-type": "error",
-    "@typescript-eslint/prefer-namespace-keyword": "error",
-    "@typescript-eslint/quotes": ["error", "single"],
-    "@typescript-eslint/semi": ["error", "always"],
-    "@typescript-eslint/triple-slash-reference": [
-      "error",
-      {
-        path: "always",
-        types: "prefer-import",
-        lib: "always",
-      },
-    ],
-    "@typescript-eslint/type-annotation-spacing": "error",
-    "@typescript-eslint/unified-signatures": "error",
-    "arrow-body-style": "error",
-    complexity: "off",
-    "constructor-super": "error",
-    curly: "error",
-    "eol-last": "error",
-    eqeqeq: ["error", "smart"],
-    "guard-for-in": "error",
-    "id-blacklist": [
-      "error",
-      "any",
-      "Number",
-      "number",
-      "String",
-      "string",
-      "Boolean",
-      "boolean",
-      "Undefined",
-      "undefined",
-    ],
-    "id-match": "error",
-    "import/no-deprecated": "warn",
-    "jsdoc/check-alignment": "error",
-    "jsdoc/check-indentation": "error",
-    "jsdoc/newline-after-description": "error",
-    "jsdoc/no-types": "off",
-    "max-classes-per-file": "off",
-    "max-len": [
-      "error",
-      {
-        code: 140,
-      },
-    ],
-    "new-parens": "error",
-    "no-bitwise": "error",
-    "no-caller": "error",
-    "no-cond-assign": "error",
-    "no-console": "off",
-    "no-debugger": "error",
-    "no-empty": "off",
-    "no-eval": "error",
-    "no-fallthrough": "error",
-    "no-invalid-this": "off",
-    "no-new-wrappers": "error",
-    "no-restricted-imports": ["error", "rxjs/Rx"],
-    "no-throw-literal": "error",
-    "no-trailing-spaces": "error",
-    "no-undef-init": "error",
-    "no-underscore-dangle": "error",
-    "no-unsafe-finally": "error",
-    "no-unused-labels": "error",
-    "no-var": "error",
-    "object-shorthand": "error",
-    "one-var": ["off", "never"],
-    "prefer-arrow/prefer-arrow-functions": "error",
-    "prefer-const": "off",
-    "quote-props": ["error", "as-needed"],
-    radix: "error",
-    "space-before-function-paren": "off",
-    "spaced-comment": [
-      "error",
-      "always",
-      {
-        markers: ["/"],
-      },
-    ],
-    "use-isnan": "error",
-    "valid-typeof": "off",
-    "@typescript-eslint/tslint/config": [
-      "error",
-      {
-        rules: {
-          "import-spacing": true,
-          typedef: [true, "call-signature"],
-          whitespace: [
-            true,
-            "check-branch",
-            "check-decl",
-            "check-operator",
-            "check-separator",
-            "check-type",
-            "check-typecast",
-          ],
-        },
-      },
-    ],
-  },
+      extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+    },
+  ],
 };
