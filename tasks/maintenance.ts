6fb85b0f68aef8f8d133f7dbd8f02599f6f2c569
Dibo <sh.eldeeb.2010@gmail.com>
2022-06-06 14:20:50 +0000

    build: tasks/maintenance

--- tasks/maintenance.ts
+++ tasks/maintenance.ts
@@ -1,27 +1,43 @@
-import { execSync } from 'node:child_process';
+import { execSync as _execSync } from 'node:child_process';
 
 /**
- * run maintenance tasks on each push using continuos integration services
+ *
+ * @param cmd
  */
-export default function maintenance() {
-  // link local dependencies
-  execSync('npm run task link && npm i --package-lock-only', {
-    stdio: 'inherit',
-  });
-  // generate and fix build files, such as readme.md
-  execSync('npm run generate', { stdio: 'inherit' });
-  // auto fix issues in the code base, such as code style, removing unused imports, checking package.dependencies
-  // see .eslintrc.js for all linting tools
-  execSync('npm run lint', { stdio: 'inherit' });
-  // push the changed files into the main repo
-  push();
+function execSync(cmd: string) {
+  _execSync(cmd, { stdio: 'inherit' });
 }
 
 /**
- * push the changed files, ignore if package-lock.json is the only changed file
+ * run maintenance tasks on each push using continuos integration services
+ *
+ * @param options
  */
-export function push() {
-  let changed = execSync(
+export default function maintenance(options: PushOptions) {
+  // link local dependencies
+  execSync('npm run task link && npm i --package-lock-only');
+  // generate and fix build files, such as readme.md
+  execSync('npm run generate');
+  // auto fix issues in the code base, such as code style, removing unused imports, checking package.dependencies
+  // see .eslintrc.js for all linting tools
+  execSync('npm run lint');
+  // push the changed files into the main repo
+  push(options);
+}
+
+export interface PushOptions {
+  user: string;
+  email?: string;
+}
+/**
+ * push the changed files, ignore if package-lock.json is the only changed file
+ *
+ * @param options
+ */
+export function push(options: PushOptions) {
+  console.log('> pushing changed files ...');
+
+  let changed = _execSync(
     'git add . && git diff-index --cached --name-only HEAD'
   )
     .toString()
@@ -32,12 +48,15 @@ export function push() {
     );
 
   if (changed.length > 0) {
+    console.log(`changed files: ${changed.length}`);
+    console.log(` - ${changed.join('\n - ')}`);
+    options.email = options.email || `${options.user}@github.com`;
+
     // give husky executing permission
     // https://stackoverflow.com/questions/8598639/why-is-my-git-pre-commit-hook-not-executable-by-default#comment88478230_47166916
-
     execSync('shx chmod +x .husky/pre-commit');
     execSync(
-      ' git config user.name ${{ github.actor }} && git config user.email "${{ github.actor }}@github.com"'
+      `git config user.name ${options.user} && git config user.email "${options.email}"`
     );
     // use --no-verify to bypass husky hooks, as we already run lint
     execSync("git commit -m 'build: maintenance' --no-verify && git push");
