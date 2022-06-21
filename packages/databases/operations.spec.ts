93c53ffa5e89faca571dc300761844b3a0b7afc8
Dibo <sh.eldeeb.2010@gmail.com>
2022-06-21 22:59:51 +0000

    feat(databases): operations.toQueryUrl()
    [skip ci]

--- packages/databases/operations.spec.ts
+++ packages/databases/operations.spec.ts
@@ -1,6 +1,6 @@
 /* eslint-disable sort-keys */
 import { expect, test } from '@jest/globals';
-import { parse } from './operations';
+import { parse, toQueryUrl } from './operations';
 
 let operations = {
   'find:users/1?x=1&y=2': {
@@ -127,3 +127,38 @@ for (let key in operations) {
     });
   }
 }
+
+test('toQueryUrl', () => {
+  expect(toQueryUrl({ collection: 'users' } as any)).toEqual('users');
+  expect(
+    toQueryUrl({ collection: 'users', database: 'mydatabase' } as any)
+  ).toEqual('mydatabase.users');
+  expect(
+    toQueryUrl({
+      collection: 'users',
+      database: 'mydatabase',
+      operation: 'find',
+    })
+  ).toEqual('find:mydatabase.users');
+
+  expect(
+    toQueryUrl({
+      collection: 'users',
+      database: 'mydatabase',
+      operation: 'find',
+      portions: ['id=1', 'name="user"'],
+    })
+  ).toEqual('find:mydatabase.users/id=1/name="user"');
+
+  expect(
+    toQueryUrl({ collection: 'users', params: { x: 1, y: 2 } } as any)
+  ).toEqual('users?{"x":1,"y":2}');
+
+  expect(
+    toQueryUrl({
+      collection: 'users',
+      params: { x: 1, y: 2 },
+      portions: ['id=1'],
+    } as any)
+  ).toEqual('users/id=1?{"x":1,"y":2}');
+});
