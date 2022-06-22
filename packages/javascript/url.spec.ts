7d9b32e19dce815d56de084ae8fa05a4883b3193
eng-dibo <sh.eldeeb.2010@gmail.com>
2022-06-22 22:40:00 +0200

    feat(javascript): url.objectToQueryParams()

--- packages/javascript/url.spec.ts
+++ packages/javascript/url.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from '@jest/globals';
-import { queryToObject } from './url';
+import { objectToQueryParams, queryToObject } from './url';
 
 test('queryToObject()', () => {
   expect(queryToObject('?a=1&b=2&b=3&c=4&d=%5Barray%5D&e=1=2&f')).toEqual({
@@ -13,4 +13,23 @@ test('queryToObject()', () => {
     e: '1=2',
     f: '',
   });
+});
+
+test('objectToQueryParams', () => {
+  expect(objectToQueryParams({ x: 1, y: 'aa', z: undefined })).toEqual(
+    'x=1&y=aa'
+  );
+});
+
+// todo: test nested objects
+test.skip('objectToQueryParams:nested', () => {
+  expect(
+    objectToQueryParams({
+      x: 1,
+      y: 'aa',
+      z: undefined,
+      obj: { a: 1 },
+      arr: [1, 2],
+    })
+  ).toEqual('x=1&y=aa&obj.a=1&arr=1&arr=2');
 });
--- packages/javascript/url.ts
+++ packages/javascript/url.ts
@@ -1,4 +1,7 @@
+import { objectType } from './objects';
+
 /**
+ * converts query params to a plain object
  *
  * @param query
  * @returns
@@ -17,4 +20,31 @@ export function queryToObject(query: string): { [key: string]: any } {
     result[key] = value;
   }
   return result;
+}
+
+/**
+ * convert a plain object into a query param string
+ *
+ * @param object the object to be converted
+ * @returns {string} the query params string
+ */
+export function objectToQueryParams(object: { [key: string]: any }): string {
+  // https://stackoverflow.com/a/57529723/12577650
+  return Object.entries(object)
+    .filter(([key, val]) => val != undefined && val != undefined)
+
+    .map(([key, val]) => {
+      let type = objectType(val);
+
+      if (type === 'array' || type === 'object') {
+        // todo: support nested objects and arrays
+        // https://stackoverflow.com/a/56174085/12577650
+        // or use qs https://www.npmjs.com/package/qs
+      }
+      return `${encodeURIComponent(key)}=${encodeURIComponent(val)}`;
+    })
+    .join('&');
+
+  // this doesn't take care of value type
+  // return new URLSearchParams(object).toString();
 }
