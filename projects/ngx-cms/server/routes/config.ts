7241faa19d6c498fbd3c948d7ac4201507a0d049
Dibo <sh.eldeeb.2010@gmail.com>
2022-07-06 05:22:39 +0000

    fix(ngx-cms): server/routes/config
    - temprarly use nativeRequire instead of import()

--- projects/ngx-cms/server/routes/config.ts
+++ projects/ngx-cms/server/routes/config.ts
@@ -1,26 +1,29 @@
 import { resolve } from 'node:path';
 import { Request, Response } from 'express';
 
-// todo: use import('~config/*').then() from browser directly
 // add '~config' to browser/webpack -> 'ESM ../config/*'
 // in this case, config/*  must be added to express.static()
 export default (request: Request, res: Response): void => {
   let file = request.params[0];
   let filePath = resolve(__dirname, `../config/${file}`);
-  import(/* webpackIgnore: true */ filePath)
-    .then((content) => {
-      if (file === 'server/vapid' && content) {
-        // only send the publicKey
-        content = content.publicKey;
-      } else if (file.startsWith('server/')) {
-        // todo: use auth for sensitive data (specially for config/server/*)
-        throw new Error('unauthorized permission');
-      }
 
-      res.json(content);
-    })
-    .catch((error) => {
-      console.error({ error });
-      res.status(500).json({ error });
-    });
+  // todo: `nativeRequire` is deprecated, use `import(/* webpackIgnore: true */ filePath)`
+  // https://github.com/webpack/webpack/issues/15975
+  let nativeRequire = require('@engineers/webpack/native-require');
+  let content = nativeRequire(filePath);
+
+  try {
+    if (file === 'server/vapid' && content) {
+      // only send the publicKey
+      content = content.publicKey;
+    } else if (file.startsWith('server/')) {
+      // todo: use auth for sensitive data (specially for config/server/*)
+      throw new Error('unauthorized permission');
+    }
+
+    res.json(content);
+  } catch (error) {
+    console.error({ error });
+    res.status(500).json({ error });
+  }
 };
